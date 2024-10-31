7c7685aba6142f906d883400769bf766c1e14be9
tan nguyen <bivani2805@gmail.com>
2024-10-31 18:28:03 +0000

    commit change

---
+++ plane_shooter.js
@@ -0,0 +1,34 @@
+const gameState = {}
+
+function preload() {
+    this.load.image('bg', 'assets/space_bg.jpg');
+}
+
+function create() {
+    gameState.bg = this.add.image(400, 275, 'bg');
+}
+
+function update() {
+
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 550,
+    backgroundColor: "ADD8E6",
+    physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 200 },
+          enableBody: true,
+        }
+    },
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+const game = new Phaser.Game(config);
